38f22360449405f9176395ffc7c89a977075afbb
palea399 <paleaheinbru@gmail.com>
2025-02-20 12:10:48 +0000

    add requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
@@ -8,6 +9,10 @@ st.title(':cup_with_straw: Customize Your Smoothie :cup_with_straw:')
 st.write(
     """Choose the fruits you want in your custom Smoothie!
     """)
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
 
 name_on_order = st.text_input('Name of Smoothie')
 st.write('The name of your Smoothie will be: ', name_on_order)
